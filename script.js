18ebcf2162dccd28d4557a58f317027e3dc0cb18
CorentinDumery <corentin.dumery@gmail.com>
2020-12-11 14:25:53 +0100

    Update & Style

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ over = 0;
         event = event || window.event;
 
         // Use event.pageX / event.pageY here
-        var machin = document.getElementById("testtruc");
+        var machin = document.getElementById("moving_bubbles");
         machin.style.left = event.pageX;
         machin.style.setProperty("left", event.pageX-10+"px");
         machin.style.setProperty("top", event.pageY-10+"px");
